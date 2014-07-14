8bbdce690912221f89f36b264c28442dcb12504b
Austen Talbot <austentalbot@gmail.com>
2014-07-14 09:53:31 -0700

    Added console logs

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+console.log(__dirname);
 var path=__dirname+'/www';
+console.log(path);
 var port=process.env.PORT || 9000;
+console.log(port);
 connect().use(serveStatic(path)).listen(port);
